5e9bb7bc4223ea99594230bf333a5b1fe5b93aea
Ammar Arjomand <persiancookbook@gmail.com>
2018-09-08 11:48:40 +0100

    basic CRUD routes and some middleware setup

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 //set up express app
 const app = express();
+
+app.use(bodyParser.json());
+
+//intialize routes
+app.use('/api/', require('./routes/api'));
 
 //listen for requests
 app.listen(process.env.port||4000, function(){

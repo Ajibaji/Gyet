ce5a69ee6bb746804ce51ff22e0cadc57663eb06
Ammar Arjomand <persiancookbook@gmail.com>
2018-09-08 13:58:02 +0100

    error handling middleware created and added

--- index.js
+++ index.js
@@ -9,11 +9,16 @@ const app = express();
 mongoose.connect('mongodb://localhost/todo');
 mongoose.Promise = global.Promise;
 
-//intialize middleware
 app.use(bodyParser.json());
 
 //intialize routes
 app.use('/api/', require('./routes/api'));
+
+//error handling middleware
+app.use(function(err, req, res, next){
+  // console.log(err);
+  res.status(422).send({error: err.message})
+});
 
 //listen for requests
 app.listen(process.env.port||4000, function(){
--- routes/api.js
+++ routes/api.js
@@ -5,26 +5,26 @@ const Todo = require('../models/todo');
 
 
 //get a list of todos from DB
-router.get('/todos', function(req, res){
+router.get('/todos', function(req, res, next){
   console.log('GET request received');
   res.send({ type:'GET' });
 });
 
 //add a new todo to the DB
-router.post('/todos', function(req, res){
+router.post('/todos', function(req, res, next){
   Todo.create(req.body).then(function(todo){
     res.send(todo);
-  });
+  }).catch(next);
 });
 
 //update a todo in the DB
-router.put('/todos/:id', function(req, res){
+router.put('/todos/:id', function(req, res, next){
   console.log('PUT request received');
   res.send({ type:'PUT' });
 });
 
 //delete a todo from DB
-router.delete('/todos/:id', function(req, res){
+router.delete('/todos/:id', function(req, res, next){
   console.log('DELET request received');
   res.send({ type:'DELETE' });
 });

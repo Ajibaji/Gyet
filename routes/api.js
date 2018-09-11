53fddc9ef5453adfd06e06987b2ff33259048b7c
Ammar Arjomand <persiancookbook@gmail.com>
2018-09-11 13:48:41 +0100

    sample route added for heroku static testing

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const Todo = require('../models/todo');
 
-
+//tester routes
+router.get('/test', function(req, res, next){
+  res.send({
+    "item": "sample"
+  });
+});
 
 //get a list of todos from DB
 router.get('/todos', function(req, res, next){
